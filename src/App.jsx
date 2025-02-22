d6fd6289eef800d45a2192648e3d5f80780f7e3a
michaelwsd <a0416517630@icloud.com>
2025-02-22 19:19:23 +1100

    changed routes

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <NavBar />
       <main className="main-content">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/movie-app" element={<Home />} />
           <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </main>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ export function NavBar() {
     return (
         <nav className="navbar">
             <div className="navbar-brand">
-                <Link to="/">Movie App</Link>
+                <Link to="/movie-app">Movie App</Link>
             </div>
             <div className="navbar-links">
                 <Link to="/" className="nav-link">Home</Link>
